3246e9c1c44a09a1b3f51f079bd4454b2e92d627
Jeonghwan-Choi <yzon007@naver.com>
2021-12-24 00:12:08 +0900

    mongoDB setting

--- node/index.js
+++ node/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://admin:chlwjdghks12@cluster0.jslnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+  //useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false -- 몽구스 버전이 6.0이상이면 항상지원
+  
+}).then(() => console.log('MongoDB Connection'))
+  .catch(err => console.log(err))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
